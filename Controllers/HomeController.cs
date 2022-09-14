75be7ac6da82cce020cbd8bedcbb6a1ab8eb8294
deveshrattan <deveshrattansharma@gmail.com>
2022-09-14 05:33:57 +0000

    firstcommit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using ShoppingTraining.Models;
+using System.Linq;
+using System.Web;
+using ShoppingTraining.Data;
+using System.Web.Mvc;
+
+namespace ShoppingTraining.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View(ProductContext.Products);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
